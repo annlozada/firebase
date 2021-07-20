cf08f414884b42416ee78c235e318af4530e9a32
Luis Angel <luismx1123@gmail.com>
2021-07-19 23:14:57 -0500

    new folder

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
+ * configuración en tu servidor de Firebase.  ana laurs*/
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyChJLNREsr1Ad4BaLvGuqJY-iaLX5ls3q0",
